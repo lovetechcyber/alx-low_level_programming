57705dedd2ec66a97a26c725fe61030611b6839b
lovetechcyber <lovetechcyb@gmail.com>
2023-09-14 05:29:05 -0700

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,24 @@ include "function_pointers.h"
  *
  * Return: no return.
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned int i;
+void array_iterator(int *array, size_t size, void (*action)(int)) {
+    for (size_t i = 0; i < size; i++) {
+        action(array[i]);
+    }
+}
 
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+// function to be used with array_iterator
+void func(int a)
+{
+    printf("%d\n", a);
+}
+
+int main() {
+    int myArr[] = {1, 2, 3, 4, 5};
+    size_t arraySize = sizeof(myArr) / sizeof(myArray[0]);
+
+    // Using array_iterator to print each element
+    array_iterator(myArr, arraySize, func);
+
+    return 0;
 }
